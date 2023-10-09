4fcc7456965cc046571ee93ff00edf37889695d0
Rodney Xiong <XDragonBall1123@gmail.com>
2023-10-09 00:06:28 -0500

    Project 1 Documents

---
+++ js/scriptIndex.js
@@ -0,0 +1,20 @@
+//welcome message Home Page
+var user = 'User'
+var salutation = 'Hello, ';
+var greeting = salutation + user + "! Welcome to may page. Here you'll learn about the two items that I enjoy using.";
+var greetingEl = document.getElementById('greeting');
+greetingEl.textContent = greeting;
+
+
+//welcome message Home Page
+
+var greetingWatch = "Welcome to the future of wearable technology! Join us as we explore the remarkable features and innovations of the Apple Watch Ultra. Discover how this cutting-edge device combines style, functionality, and performance to enhance your everyday life.";
+var greetingEl = document.getElementById('greetingWatch');
+greetingWatchEl.textContent = greetingWatch;
+
+
+//Price of Phone
+var price = 749.49;
+    var priceElement = document.getElementById("price");
+    priceElement.textContent = price.toFixed(2);
+
---
+++ js/scriptPhone.js
@@ -0,0 +1,9 @@
+//welcome message Home Page
+var greetingPhone = "The iPhone 13 Pro Max is the largest and most expensive model in Apple's 2021 smartphone line-up and features a 6.7-inch Super Retina XDR display with 1284 x 2778 pixels resolution. Like the smaller iPhone 13 Pro, it is powered by Apple's latest A15 Bionic chipset and comes with up to 1TB of internal storage.";
+var greetingEl = document.getElementById('greetingPhone');
+
+
+// Price for iPhone 13 Pro Max
+const pricePhone = 999.99;
+var pricePhoneElement = document.getElementById("price");
+pricePhoneElement.textContent = pricePhone.toFixed(2);
